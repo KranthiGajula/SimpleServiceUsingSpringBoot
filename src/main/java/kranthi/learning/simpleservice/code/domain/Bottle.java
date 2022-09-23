c66aaec9a910aec37c134ed03e1330ffc37a50ae
Kranthi <kranthi_gajula@epam.com>
2022-09-23 14:03:18 +0200

    Added attributes and setter and getter methods.

--- src/main/java/kranthi/learning/simpleservice/code/domain/Bottle.java
+++ src/main/java/kranthi/learning/simpleservice/code/domain/Bottle.java
@@ -1,5 +1,35 @@
 package kranthi.learning.simpleservice.code.domain;
 
 public class Bottle {
+
+    private String brand;
+    private String type;
+    private String dateOfManufacture;
+    private float price;
+    
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+    public void setDateOfManufacture(String dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
     
 }
